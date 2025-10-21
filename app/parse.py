8f0b37b7b0c091b8952e63487b5a868daa1aad7c
reinvvay <reinvvay@gmail.com>
2025-10-21 21:45:34 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,9 @@
 import csv
 from dataclasses import dataclass
 
+import requests
+from bs4 import BeautifulSoup
+
 
 @dataclass
 class Quote:
@@ -10,7 +13,18 @@ class Quote:
 
 
 def main(output_csv_path: str) -> None:
-    pass
+    url = "https://quotes.toscrape.com/"
+    text = requests.get(url).content
+    soup = BeautifulSoup(text, "html.parser")
+    with open(output_csv_path, "w", encoding="UTF-8") as csvfile:
+        writer = csv.writer(csvfile)
+        for block in soup.find_all("div", class_="quote"):
+            quote = Quote(
+                text=block.find('span', class_="text").string,
+                author=block.find('small', class_="author").string,
+                tags=block.find_all('a', class_="tag")
+            )
+            writer.writerow(f"{quote.text} | {quote.author} | {[tag.string for tag in quote.tags]}")
 
 
 if __name__ == "__main__":
